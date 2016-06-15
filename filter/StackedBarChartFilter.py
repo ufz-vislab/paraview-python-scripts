ccafa144e1b4b4c430911634aa76eeb611923838
Lars Bilke <lars.bilke@ufz.de>
2016-06-15 18:16:32 +0200

    StackedBarChart correctly handles negative values.
    They create lines pointing in negative z axis direction.

--- filter/StackedBarChartFilter.py
+++ filter/StackedBarChartFilter.py
@@ -1,6 +1,6 @@
 Name = 'StackedBarChartFilter'
 Label = 'Stacked Bar Chart Filter'
-Help = 'Creates a stacked bar chart (as lines) of all point data data arrays'
+Help = 'Creates a stacked bar chart (as lines in z-direction) of all point data arrays'
 
 NumberOfInputs = 2
 InputDataType = 'vtkPolyData'
@@ -11,7 +11,6 @@ Properties = dict(
 )
 
 def RequestData():
-
     pdi = self.GetPolyDataInput()
     pdo = self.GetPolyDataOutput()
 
@@ -33,9 +32,19 @@ def RequestData():
     for i in range(0, numPoints-1):
         currentPoint = pdi.GetPoints().GetPoint(i)
         basePointId = newPoints.InsertNextPoint(currentPoint)
-        for v in range(0, numVars):
+        posIndex = 0
+        negIndex = 0
+        beginIndex = 0
+        for v in range(0, numVars-1):
             value = pdi.GetPointData().GetArray(v).GetTuple1(i)
-            newPointId = newPoints.InsertNextPoint(currentPoint[0], currentPoint[1], currentPoint[2] + value * scalingFactor)
-            linepoints = [basePointId + v, newPointId]
-            pdo.InsertNextCell(3, 2, linepoints) # VTK_LINE is 3
+            if value < 0:
+                beginIndex = i * numVars + negIndex
+                negIndex = v + 1
+            else:
+                beginIndex = i * numVars + posIndex
+                posIndex = v + 1
+
+            beginPoint = newPoints.GetPoint(beginIndex)
+            newPointId = newPoints.InsertNextPoint(beginPoint[0], beginPoint[1], beginPoint[2] + value * scalingFactor)
+            pdo.InsertNextCell(3, 2, [beginIndex, newPointId]) # VTK_LINE is 3
             valIdArray.SetValue(i*numVars + v, v)
