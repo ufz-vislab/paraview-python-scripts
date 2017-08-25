b2dfbdd1507c21f0e8eaab474077a60d59d03b8d
Lars Bilke <larsbilke83@googlemail.com>
2017-08-25 11:17:05 +0200

    Added options specifying output directory timestep export control.

--- scripts/time/ExportTimesteps.py
+++ scripts/time/ExportTimesteps.py
@@ -1,20 +1,31 @@
-# author: Carolin Helbig 2013-07-23
+# author: Carolin Helbig 2013-07-23, Lars Bilke
 # A source object has to be selected in the pipeline browser!
+# On first input dialog specify export directory.
+# On second input dialog specify nth timesteps to export, 1 for exporting all.
+import os, math
 try: paraview.simple
 except: from paraview.simple import *
 paraview.simple._DisableFirstRenderCameraReset()
 
-exporters=servermanager.createModule("exporters")
+exporters = servermanager.createModule("exporters")
 
 RenderView1 = GetRenderView()
 reader = GetActiveSource()
 tsteps = reader.TimestepValues
+numtsteps = len(tsteps)
+numdigits = int(math.ceil(math.log10(numtsteps)))
 
+dir = raw_input("Export directory: ")
+assert os.path.exists(dir), "Directory does not exist "+str(dir)
+step = raw_input("Export every nth timestep: ")
+if step == '':
+    step = 1
 
-for index in range(len(tsteps)):
+for index in range(0, numtsteps, int(step)):
     RenderView1.ViewTime = tsteps[index]
     Render()
     view = GetActiveView()
-    FbxExporter=exporters.FbxExporter(FileName="C:/Users/vislab/unity/all/Assets/_project/Bayrischer Bahnhof/Meshes/sim-ost/Temp-1.5/" + str(index) + ".fbx")
+    FbxExporter = exporters.FbxExporter(FileName=str(dir) + "/" + str(index).zfill(numdigits) + ".fbx")
     FbxExporter.SetView(view)
     FbxExporter.Write()
+    
